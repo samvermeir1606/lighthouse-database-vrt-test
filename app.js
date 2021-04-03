83b0a37f075d06fb21a9a06a3c2693e43d74e6c7
Sam Vermeir <sam.vermeir@hotmail.com>
2021-04-03 15:07:30 +0200

    added connection to database

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+const { Client } = require('pg');
+//
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+//
+client.connect();
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
