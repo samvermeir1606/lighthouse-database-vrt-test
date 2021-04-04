ceb16532e0d924967bbd412f2318816c247ef018
Sam Vermeir <sam.vermeir@hotmail.com>
2021-04-04 15:06:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/testing/fetchingurl',function(req,res){
 	  	res.send("DONE")
 		var formatedMysqlString = (new Date ((new Date((new Date(new Date())).toISOString() )).getTime() - ((new Date()).getTimezoneOffset()*60000))).toISOString().slice(0, 19).replace('T', ' ');
 
-		client.query("INSERT INTO scores(websiteurl, score, date, mainbrand) VALUES ('https://www.vrt.be/vrtnu/', "+data.lighthouseResult.categories.accessibility.score+",'"+formatedMysqlString +"','VRT');", (err, outcome) => {   
+		client.query("INSERT INTO scores(websiteurl, score, date, mainbrand) VALUES ('https://www.vrt.be/vrtnu/', "+data.lighthouseResult.categories.accessibility.score*100+",'"+formatedMysqlString +"','VRT');", (err, outcome) => {   
 			if (err) throw err;
 			else {
 				//res.send(outcome)
