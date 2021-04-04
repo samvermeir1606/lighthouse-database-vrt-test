e81591662296472abd8afcd7694706b98be37e25
Sam Vermeir <sam.vermeir@hotmail.com>
2021-04-04 14:27:29 +0200

    changed date

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/testing/fetchingurl',function(req,res){
 	  	console.log(data.lighthouseResult.categories.accessibility.score)
 	  	res.send("DONE")
 
-		client.query("INSERT INTO scores(websiteurl, score, date, mainbrand) VALUES ('https://www.vrt.be/vrtnu/', "+data.lighthouseResult.categories.accessibility.score+",'"+Date()+"','VRT');", (err, outcome) => {   
+		client.query("INSERT INTO scores(websiteurl, score, date, mainbrand) VALUES ('https://www.vrt.be/vrtnu/', "+data.lighthouseResult.categories.accessibility.score+",'"+Date().toMysqlFormat() +"','VRT');", (err, outcome) => {   
 			if (err) throw err;
 			else {
 				res.send(outcome)
@@ -85,4 +85,28 @@ app.get('/testing/showall',function(req,res){
 })
 
 
+
+
+
+
+
+
+/**
+ * You first need to create a formatting function to pad numbers to two digits…
+ **/
+function twoDigits(d) {
+    if(0 <= d && d < 10) return "0" + d.toString();
+    if(-10 < d && d < 0) return "-0" + (-1*d).toString();
+    return d.toString();
+}
+
+/**
+ * …and then create the method to output the date string as desired.
+ * Some people hate using prototypes this way, but if you are going
+ * to apply this to more than one Date object, having it as a prototype
+ * makes sense.
+ **/
+Date.prototype.toMysqlFormat = function() {
+    return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
+};
 
