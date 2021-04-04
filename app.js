e466f0b5e3645c821c2c7d9fe3767cb1be87fff9
Sam Vermeir <sam.vermeir@hotmail.com>
2021-04-04 12:32:12 +0200

    added xml requests

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const { Client } = require('pg');
 //
