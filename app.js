f7b0e54a94b36c3dd4e6e96457313916727d9321
Sam Vermeir <sam.vermeir@hotmail.com>
2021-04-04 13:27:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -29,21 +29,22 @@ app.listen(process.env.PORT || port, () => {
 // Add Row To Database
 app.get('/testing/:websiteurl',function(req,res){
 	console.log("NEW Testing websiteurl");
-
-	// Create a request variable and assign a new XMLHttpRequest object to it.
 	var request = new XMLHttpRequest()
 	
-	// Open a new connection, using the GET request on the URL endpoint
 	request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-	
 	request.onload = function () {
-		console.log("OnLoad function");
 	  // Begin accessing JSON data here
-		var data = JSON.parse(this.response)
-		console.log(data.lighthouseResult.categories.accessibility.score)
+	  var data = JSON.parse(this.response)
+	
+	  if (request.status >= 200 && request.status < 400) {
+	    data.forEach((movie) => {
+	      console.log(movie.title)
+	    })
+	  } else {
+	    console.log('error')
+	  }
 	}
 	
-	// Send request
 	request.send()
 
 
